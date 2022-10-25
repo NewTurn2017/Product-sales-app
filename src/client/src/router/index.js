6edf61bad707b8ec4004c6abcc26584e3b43698b
jaehyunjang <hyuni2020@gmail.com>
2022-10-25 11:52:43 +0900

    Router 기본 연결

--- src/client/src/router/index.js
+++ src/client/src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProductList from '../views/ProductList.vue'
+import ProductDetail from '../views/ProductDetail.vue'
+import ProductCreate from '../views/ProductCreate.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: ProductList
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/detail',
+    name: 'ProductDetail',
+    component: ProductDetail
+  },
+  {
+    path: '/create',
+    name: 'ProductCreate',
+    component: ProductCreate
   }
 ]
 
