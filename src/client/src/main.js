e34e9b69fb7162474336918a0e07ecfe7dfb305e
jaehyunjang <hyuni2020@gmail.com>
2022-10-25 14:31:56 +0900

    Bootstrap 최신 CDN index.html 에 적용

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 import mixins from './mixins'
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/main.css'
 
 createApp(App).use(store).use(router).mixin(mixins).mount('#app')
