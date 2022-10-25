f8c79d3d361d3c516028b1bae39bce550d9d7236
jaehyunjang <hyuni2020@gmail.com>
2022-10-25 12:56:32 +0900

    페이지 router 완성

--- src/client/src/views/ProductList.vue
+++ src/client/src/views/ProductList.vue
@@ -1,48 +1,5 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Soldout</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#">홈</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="#">전자제품</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">의류</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">식품</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
     <main class="mt-3">
       <div class="container">
         <div class="row mb-2">
